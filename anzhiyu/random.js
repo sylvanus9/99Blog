e3bb81be440086c737cbba5d721c4a2af2447bdb
Ethereal <2328800101@qq.com>
2025-06-13 20:41:15 +0800

    Site updated: 2025-06-13 20:41:15

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2022/11/19/example/","2023/01/18/linggan/纸面情绪/","2023/03/10/linggan/柯南/"];function toRandomPost(){
+var posts=["2023/03/10/linggan/柯南/","2022/11/19/example/","2023/01/18/linggan/纸面情绪/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
