86a28c056b51d56d2fd302b4055f66a921e20cbc
Ethereal <2328800101@qq.com>
2025-06-16 13:34:36 +0800

    Site updated: 2025-06-16 13:34:35

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/11/09/日常碎片/最近的我/","2025/06/13/直播笔记/【抖音】粉丝地址查找 copy/","2025/06/16/直播笔记/【抖音】LX战队投票指南/"];function toRandomPost(){
+var posts=["2025/06/16/直播笔记/【抖音】LX战队投票指南/","2024/11/09/日常碎片/最近的我/","2025/06/13/直播笔记/【抖音】粉丝地址查找 copy/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
